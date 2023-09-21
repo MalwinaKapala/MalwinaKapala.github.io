72ccbfc6437f95d8677989ecfc65cb9ae1f30b1c
Malwina Kapala <malwina.gadek@gmail.com>
2023-09-21 21:21:45 +0200

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('dla mamy i dziecka');
+	const posts = await getCollection('wiersze');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
