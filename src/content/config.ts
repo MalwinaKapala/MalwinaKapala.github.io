fabc03ba30b4346b74afa3d5ab6f39962a76092f
Pawel Kapala <github@kapware.com>
2023-09-29 23:00:05 +0200

    Added collection ptaki

--- src/content/config.ts
+++ src/content/config.ts
@@ -36,4 +36,23 @@ const dlamamyidziecka = defineCollection({
 	}),
 });
 
-export const collections = { wiersze, dlamamyidziecka };
+const ptaki = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+	}),
+});
+
+
+export const collections = { wiersze, dlamamyidziecka, ptaki };
