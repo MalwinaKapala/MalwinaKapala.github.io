66975536d6754b6d679df612200e9d940f86f6c4
Pawel Kapala <github@kapware.com>
2023-11-17 11:16:05 +0100

    Fixed development mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: import.meta.env.MODE === 'development' ? 'http://localhost:4321' : 'https://wierszemalwiny.pl',
+	site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://wierszemalwiny.pl',
 	integrations: [mdx(), sitemap()],
 });
